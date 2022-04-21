56bf2cdf58fe356e0e73d4b11787138dfbc51979
Craig Hughes <hghscraig@gmail.com>
2022-04-22 00:34:53 +0100

    Match play grid to activeBlock position

--- src/components/Game.ts
+++ src/components/Game.ts
@@ -11,6 +11,7 @@ class Game {
     height: number;
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
+    grid: number[];
     activeBlock: Block | null;
 
     constructor(
@@ -29,7 +30,12 @@ class Game {
         this.height = height;
         this.canvas = document.getElementById('screen') as HTMLCanvasElement;
         this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
+        this.grid = new Array(
+            (this.height / this.blockSize) * (this.width / this.blockSize)
+        ).fill(0);
         this.activeBlock = null;
+
+        console.log(this.grid);
 
         this.setup();
     }
@@ -56,9 +62,46 @@ class Game {
         this.ctx.clearRect(0, 0, this.width, this.height);
     }
 
+    check() {
+        if (!this.activeBlock) return;
+
+        for(const [i, v] of Object.entries(this.activeBlock.shape)) {
+            if (v !== 0) {
+                const xToCheck = (this.activeBlock.pos.x / this.blockSize);
+                const yToCheck = (this.activeBlock.pos.y / this.blockSize);
+                const yBreak = Math.floor((this.width / this.blockSize));
+                const yOffset = (Math.floor(parseInt(i) / 4) * yBreak);
+                const startPoint = (yToCheck * yBreak) + xToCheck;
+
+                // this.grid[(startPoint + Math.floor(parseInt(i) % 4) + yOffset)] = v;
+            }
+        }
+    }
+
     draw() {
         this.ctx.fillStyle = 'black';
         this.ctx.fillRect(0, 0, this.width, this.height);
+
+        const colors = ['black', 'orange', 'blue', 'red', 'green', 'cyan', 'purple', 'yellow'];
+
+        for(const [i, v] of Object.entries(this.grid)) {
+            if (v === 0) continue;
+
+            const xOffset = Math.floor((this.height / this.blockSize));
+            const yOffset = Math.floor((this.width / this.blockSize));
+            this.ctx.fillStyle = colors[v];
+            this.ctx.fillRect(
+                Math.floor(parseInt(i) % yOffset) * this.blockSize, 
+                Math.floor(parseInt(i) / yOffset) * this.blockSize, 
+                this.blockSize, 
+                this.blockSize
+            );
+
+            // this.ctx.fillStyle = 'black';
+            // this.ctx.fillText(i, Math.floor(parseInt(i) % yOffset) * this.blockSize, 
+            // Math.floor(parseInt(i) / yOffset) * this.blockSize);
+
+        }
     }
     
     main() {
@@ -66,6 +109,7 @@ class Game {
 
         if (!this.activeBlock || this.activeBlock?.placed) this.activeBlock = new Block(this.canvas, this.ctx, this.blockSize);
         this.activeBlock.draw();
+        this.check();
 
         if (this.activeBlock.pos.y + (this.blockSize * 2) < (this.canvas.height - this.blockSize)) this.activeBlock.pos.y += this.blockSize;
         else this.activeBlock.placed = true;
